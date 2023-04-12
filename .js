7c5958f660a67d1f9563e055d61c3c3750516c27
MabundaDee <shaydmitry232@gmail.com>
2023-04-12 12:17:14 +0200

    All remaining files

---
+++ .js
@@ -0,0 +1,12 @@
+function GetData(){
+
+    document.getElementById("fname").value;
+    document.getElementById("lname").value;
+    document.getElementById("gender").value;
+    document.getElementById("email").value;
+    
+}
+
+function getData(e) {
+    e.preventDefault();
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+function GetData(){
+
+let fname =  document.getElementById("Firstname").value;
+    let lname=document.getElementById("Lastname").value;
+    let gender=document.getElementById("Gender").value;
+    let email=document.getElementById("Email Address").value;
+    let Nationality = document.getElementById("Nationality").value
+    console.log ("First name:"+ Firstname,"Last name :"+Lastname,"Gender :"+ gender,"Email Address :"+ email, "Nationality :" + Nationality )
+
+    
+}
+
+
+
